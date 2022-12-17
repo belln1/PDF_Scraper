e19135ac863aa07f54581bac56bff52962329237
belln1 <bell@zhaw.ch>
2022-12-17 15:33:32 +0100

    xx

--- src/pdfscraper/PdfScraper.java
+++ src/pdfscraper/PdfScraper.java
@@ -34,6 +34,7 @@ public class PdfScraper {
 
 	static String delimiter = ";";
 	//String urlname = "http://eur-lex.europa.eu/legal-content/DE/TXT/PDF/?uri=CELEX:31972D0026&from=EN";
+	//xx
 
 
 	public static void main(String[] args) {
